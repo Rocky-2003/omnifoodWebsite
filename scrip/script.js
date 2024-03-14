3847f6bb44969fc569d7993ee99d950540335bc2
Rocky Mor <rockymor2003@gmail.com>
2024-03-14 21:40:25 +0530

    done mobile navigation

---
+++ scrip/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const btn = document.querySelector(".btn-mobile-nav");
+const menuBar = document.querySelector(".header");
+
+btn.addEventListener("click", (e) => {
+  // e.preventDefault();
+  console.log("Rocky Mor");
+
+  menuBar.classList.toggle("nav-open");
+});
